49b54714df2f49a5ff4591e5ecc2f7c5226cbd7c
smileHJdev <smilehj.dev@gmail.com>
2022-07-16 00:39:36 +0900

    set .env

--- part7/ex7-1.py
+++ part7/ex7-1.py
@@ -1,7 +1,20 @@
+from dotenv import load_dotenv
+import os
 import pymysql
 
-db = pymysql.connect(
 
+load_dotenv()
+HOST = os.environ.get("HOST")
+PORT = int(os.environ.get("PORT"))
+USER_NAME = os.environ.get("USER_NAME")
+PASSWD = os.environ.get("PASSWD")
+
+
+db = pymysql.connect(
+    host=HOST,
+    port=PORT,
+    user=USER_NAME,
+    passwd=PASSWD,
     db='chatbotDB',
     charset='utf8'
 )
--- part7/ex7-2.py
+++ part7/ex7-2.py
@@ -1,9 +1,21 @@
+from dotenv import load_dotenv
+import os
 import pymysql
+
+
+load_dotenv()
+HOST = os.environ.get("HOST")
+PORT = int(os.environ.get("PORT"))
+USER_NAME = os.environ.get("USER_NAME")
+PASSWD = os.environ.get("PASSWD")
 
 db = None
 try:
     db = pymysql.connect(
-
+        host=HOST,
+    port=PORT,
+    user=USER_NAME,
+    passwd=PASSWD,
         db='chatbotDB',
         charset='utf8'
     )
