66ac71cd3578eee72988e863e7bf2947d8cb2c2c
smileHJdev <smilehj.dev@gmail.com>
2022-07-21 22:24:38 +0900

    ended 8-5

--- utils/Preprocess.py
+++ utils/Preprocess.py
@@ -1,8 +1,16 @@
 from konlpy.tag import Komoran
+import pickle
 
 #전처리 과정 시작
 class Preprocess:
-    def __init__(self, userdic=None):  # 생성자
+    def __init__(self, word2index_dic='',userdic=None):  # 생성자
+        #단어 인덱스 사전 불러오기
+        if(word2index_dic !=''):
+            f = open(word2index_dic, "rb")
+            self.word_index = pickle.load(f)
+            f.close()
+        else:
+            self.word_index = None
         # 형태소 분석기 초기화
         self.komoran = Komoran(userdic=userdic)
 
@@ -29,3 +37,14 @@ class Preprocess:
             if f(p[1]) is False:
                 word_list.append(p if without_tag is False else p[0])
         return word_list
+    #키워드를 단어 인덱스 시퀀스로 변환
+    def get_wordidx_sequence(self, keywords):
+        if self.word_index is None:
+            return []
+        w2i = []
+        for word in keywords:
+            try:
+                w2i.append(self.word_index[word])
+            except KeyError:
+                #해당 단어가 사전에 없는경우 OOV처리
+            return w2i
