68ee6da8ac09488dc00f17a127ae5b263013ec7b
smileHJdev <smilehj.dev@gmail.com>
2022-07-22 10:08:28 +0900

    ended 8-5

--- utils/Preprocess.py
+++ utils/Preprocess.py
@@ -37,14 +37,17 @@ class Preprocess:
             if f(p[1]) is False:
                 word_list.append(p if without_tag is False else p[0])
         return word_list
-    #키워드를 단어 인덱스 시퀀스로 변환
+        # 키워드를 단어 인덱스 시퀀스로 변환
+
     def get_wordidx_sequence(self, keywords):
         if self.word_index is None:
             return []
+
         w2i = []
         for word in keywords:
             try:
                 w2i.append(self.word_index[word])
             except KeyError:
-                #해당 단어가 사전에 없는경우 OOV처리
-            return w2i
+                # 해당 단어가 사전에 없는 경우, OOV 처리
+                w2i.append(self.word_index['OOV'])
+        return w2i
